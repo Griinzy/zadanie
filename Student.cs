4e242580d5b83440e287baaa5001be5fee08bbda
griinzy <kalin_stoqnov@abv.bg>
2024-02-29 17:06:49 +0200

    Upload

---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    public class Students
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Students(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public string GetFullName() 
+        { 
+            return FirstName+" "+LastName;
+        }
+        public bool isAdult()
+        { 
+            return Age >= 18;
+        }
+    }
+}
